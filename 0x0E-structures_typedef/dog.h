d655c16364f00ee8d3978fbef9f5c6e4ebfe1fe4
Mahamat Nour Bourma <mnbourma@outlook.com>
2023-01-16 09:36:59 -0800

    Define a new type struct dog with name, age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef UNIQUE_STRUCT
+#define UNIQUE_STRUCT
+/**
+ * struct dog - defines a dog.
+ * @name: this the name of the dog.
+ * @age: this is the age of the dog.
+ * @owner: this the owner of the dog.
+ * Description: the given structure defines a dog with some basic infromations.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
