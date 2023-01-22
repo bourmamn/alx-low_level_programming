9b247dde4fdcb27dd08e112d061e36334713ae33
Mahamat Nour Bourma <mnbourma@outlook.com>
2023-01-22 11:49:41 -0800

    Function that returns the sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,6 +3,7 @@
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: this is the first parameter.
+ * Description: this function gives the sum of its parameters and 0 if n is 0.
  * Return: sum of all given parameters.
  */
 int sum_them_all(const unsigned int n, ...)
